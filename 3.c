35f3cdb54ddb51f2cb0e679f319724dd90a01b60
surya135 <35728499+surya135@users.noreply.github.com>
2018-02-01 16:45:04 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,16 @@
+
+int main()
+{
+int i,j;
+    printf("enter the word
+#include <stdio.h>");
+    for(i=0;i<=1;i++)
+    {
+        for(j=0;j<=1;j++)
+        printf("HELLO");
+        
+    }
+    
+    
+    return 0;
+}
