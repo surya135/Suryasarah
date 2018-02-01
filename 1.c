9b5d8056805d70a8dd3acf27ee813722239f57c2
surya135 <35728499+surya135@users.noreply.github.com>
2018-02-01 16:04:37 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
-#include <conio.h>
-  
-int main(){
+int main()
+{
     char c;
-    printf("Enter the Character: ");
-    scanf("%c", &c);
-    switch(c)
-    {
-        case 'A':
-        case 'E':
-        case 'I':
-        case 'O':
-        case 'U':
-        case 'a':
-        case 'e':
-        case 'i':
-        case 'o':
-        case 'u':
-            printf("%c    Vowel \n", c);
-            break;
-        default:
-            printf("%c  Consonant\n", c);
-    }    
-    getch();
+    int isLowercaseVowel, isUppercaseVowel;
+
+    printf("Enter an alphabet: ");
+    scanf("%c",&c);
+
+  
+    isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+
+    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+    if (isLowercaseVowel || isUppercaseVowel)
+        printf("%c  vowel.", c);
+    else
+        printf("%c consonant.", c);
     return 0;
 }
