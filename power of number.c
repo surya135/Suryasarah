9cea4aba3a2ef98d4232fb05bc1cd8437e127a13
surya135 <35728499+surya135@users.noreply.github.com>
2018-02-06 16:36:33 +0530

    Create power of number.c

---
+++ power of number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int s,u,r;
+    printf("enter the values:");
+    scanf("%d",&s,&u);
+    r=pow(s,u);
+    printf("%d",r);
+    return 0;
+}
