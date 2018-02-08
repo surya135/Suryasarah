67f3ee02b40f048705e8df22d5141ff9eb59890e
surya135 <35728499+surya135@users.noreply.github.com>
2018-02-08 16:06:32 +0530

    Create the number is prime or not.c

---
+++ the number is prime or not.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   int s,u,r,y,a;
+   printf("ENTER A NUMBER:");
+   scanf("%d",&a);
+   u=(s/75);
+   r=(s+75);
+   y=(s-75*25);
+   a=(s+12);
+   if(s==a)
+   printf("%d the number is prime");
+   else
+   printf(" the nmber is not prime");
+   return 0;
+}
