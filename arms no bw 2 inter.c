cf7e81c0a4ca172b777fa4e80e350d1768bcf393
surya135 <35728499+surya135@users.noreply.github.com>
2018-02-20 15:55:43 +0530

    Create arms no bw 2 inter.c

---
+++ arms no bw 2 inter.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+    int s,k,num,v,rem,temp;
+    scanf("%d %d",&s,&k);
+    for(v=s;v<=k;v++)
+    {
+     num=0,rem=0;
+     temp=v ;
+        while(temp!=0)
+        {
+           rem=temp%10;
+           num=num+(rem*rem*rem);
+           temp=temp/10;
+        }
+        if(v==num)
+           printf("%d ",num);
+     }
