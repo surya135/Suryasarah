716bde9768f933c61f63ae279e874ba30a7a394d
surya135 <35728499+surya135@users.noreply.github.com>
2018-03-01 16:14:38 +0530

    Create array of n int min.c

---
+++ array of n int min.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int s,k,n,p[15],temp;
+    clrscr();
+    printf("Enter the array size\t");
+    scanf("%d\n",&n);
+    for(s=0;s<n;s++)
+    {
+        scanf("%d\t",&p[s]);
+    }
+    for(s=0;s<n-1;s++)
+    {
+        for(k=s+1;k<n;k++)
+        {
+            if(p[s]>p[k])
+            {
+                temp=p[s];
+                p[s]=p[k];
+                p[k]=temp;
+            }
+        }
+    }
+    printf("%d",p[0]);
+    getch();
+
+}
